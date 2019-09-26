4a3dfbcf7e2a149a71db5bf7c75b88ae06932f24
Guido Sangiovanni <sangiovanni.guido@gmail.com>
2019-09-26 09:50:31 +0200

    Progetto funzionante, aggiunta index

--- public/index.php
+++ public/index.php
@@ -1,2 +1,22 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+require_once('../vendor/autoload.php');
+
+$app = new Silex\Application();
+
+$app->get('/', function(Request $request) {
+        return new Response(
+            '<html>
+        <body style="
+            background-color: black; 
+            color: white;">
+                Ciao!
+            </body>
+        </html>'
+        );
+});
+
+$app->run();
