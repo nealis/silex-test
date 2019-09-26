15ccdc6f70e6871c7d841ea7750e7a79385c9ac9
sangio89 <sangiovanni.umberto@yahoo.it>
2019-09-26 14:39:28 +0200

    Aggiunto Doctrine

--- public/index.php
+++ public/index.php
@@ -7,9 +7,38 @@ use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
 
 require_once('../vendor/autoload.php');
-
 $app = new Application();
 $app['password'] = 'guido';
+
+
+$user = 'root';
+$password = 'root';
+$db = 'libri';
+$host = 'localhost';
+$port = 8889;
+
+$connection = mysqli_connect(
+   $host,
+   $user,
+   $password,
+   $db,
+   $port
+);
+
+$app->register(new TwigServiceProvider(), [
+    'twig.path' => __DIR__ .'/../views/',
+]);
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'   => 'pdo_mysql',
+        'host' => 'localhost',
+        'dbname' => 'asdasdsad',
+        'user' => 'root',
+        'password' => 'root',
+        'port' => 8889,
+    ),
+));
 
 //$app->before(function (Request $request, Application $app) {
 //    $password = $request->headers->get('password');
@@ -21,15 +50,26 @@ $app['password'] = 'guido';
 //        return new Response('PASSWORD ERRATA!');
 //    }
 //});
+$twig = $app['twig'];
+$db = $app['db'];
 
-$app->get('/', function(Request $request) use ($app) {
-    /** @var Environment $twig */
-    $twig = $app['twig'];
+$app->get('/', function(Request $request) use ($twig, $db) {
+    
+    $sql = "SELECT * FROM libri";
+
+    $libri = $db->fetchAll($sql);
+    foreach ($libri as $libro) {
+        echo $libro['title']."</br> ";
+    }
     return new Response(
         $twig->render(
             'index.twig'
         )
     );
+});
+
+$app->get('/libri', function (request $request) use ($app) {
+
 });
 
 $app->get('/salutami/{nomeUtente}', function(Request $request, $nomeUtente) use ($app) {
@@ -44,8 +84,5 @@ $app->get('/salutami/{nomeUtente}', function(Request $request, $nomeUtente) use 
     );
 });
 
-$app->register(new TwigServiceProvider(), [
-    'twig.path' => __DIR__ .'/../views/',
-]);
 
 $app->run();
