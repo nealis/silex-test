64afdcd0c3415d9ca22c5637e535f62d7fd791d1
Guido Sangiovanni <sangiovanni.guido@gmail.com>
2019-09-26 16:03:56 +0200

    Aggiunta gestione namespace Libreria, creato Controller

---
+++ src/Libreria/Controller/LibriController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Libreria\Controller;
+
+use Silex\Application;
+
+class LibriController
+{
+
+    /** @var Application */
+    protected $app;
+    /**
+     * LibriController constructor.
+     */
+    public function __construct()
+    {
+    }
+
+}
