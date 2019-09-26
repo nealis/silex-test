ac44f33266a6312557ee1f6bc70f4ebb6ab67c74
Guido Sangiovanni <sangiovanni.guido@gmail.com>
2019-09-26 10:28:04 +0200

    Aggiunto Twig, commentato Middleware, aggiunta rotta dinamica per salutare gli utenti

--- public/index.php
+++ public/index.php
@@ -1,22 +1,51 @@
 <?php
 
+use Silex\Application;
+use Silex\Provider\TwigServiceProvider;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
 require_once('../vendor/autoload.php');
 
-$app = new Silex\Application();
+$app = new Application();
+$app['password'] = 'guido';
 
-$app->get('/', function(Request $request) {
-        return new Response(
-            '<html>
-        <body style="
-            background-color: black; 
-            color: white;">
-                Ciao!
-            </body>
-        </html>'
-        );
+//$app->before(function (Request $request, Application $app) {
+//    $password = $request->headers->get('password');
+//    if (empty($password)) {
+//        return new Response('PASSWORD MANCANTE');
+//    }
+//
+//    if ($password !== $app['password']) {
+//        return new Response('PASSWORD ERRATA!');
+//    }
+//});
+
+$app->get('/', function(Request $request) use ($app) {
+    /** @var Environment $twig */
+    $twig = $app['twig'];
+    return new Response(
+        $twig->render(
+            'index.twig'
+        )
+    );
 });
+
+$app->get('/salutami/{nomeUtente}', function(Request $request, $nomeUtente) use ($app) {
+    /** @var Environment $twig */
+    $twig = $app['twig'];
+    return new Response(
+        $twig->render(
+            'saluto.twig', [
+                'nomeUtente' => $nomeUtente
+            ]
+        )
+    );
+});
+
+$app->register(new TwigServiceProvider(), [
+    'twig.path' => __DIR__ .'/../views/',
+]);
 
 $app->run();
